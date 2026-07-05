62682a5fc63361a9cc7f0c01be84cd9ea0ed3baf
J-dar Siegfred Rodriguez <siegfred.rodriguez@gmail.com>
2026-07-05 11:01:47 +0800

    fix: add SELECT policy on storage.buckets for anon

--- supabase/migrations/00006_storage_buckets.sql
+++ supabase/migrations/00006_storage_buckets.sql
@@ -3,6 +3,12 @@ INSERT INTO storage.buckets (id, name, public)
 VALUES ('account-icons', 'account-icons', true)
 ON CONFLICT (id) DO NOTHING;
 
+-- Allow anon to see the bucket
+CREATE POLICY "anon select account-icons bucket"
+ON storage.buckets FOR SELECT
+TO anon
+USING (id = 'account-icons');
+
 -- Allow anon to upload files
 CREATE POLICY "anon upload account icons"
 ON storage.objects FOR INSERT
